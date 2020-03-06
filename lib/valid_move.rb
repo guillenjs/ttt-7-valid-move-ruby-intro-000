960ec1d7b42d569d340465dd0ac783917719bad0
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-03-06 22:05:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,7 @@ if index.between?(1,9)
   if !position_taken?(board,index)
     true
   end
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
